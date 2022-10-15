06cf877d0e58fee17cc8f22506703f624793903e
Bobson Obiku <leonineobiku@gmail.com>
2022-10-14 18:22:12 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdoi.h>
+/**
+ * main - Entry point
+ * Description: 'size of data'
+ * Return:0
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte\n", sizeof(char));
+	printf("Size of int: %i bytes\n", sizeof(int));
+	printf("Size of long int: %i bytes\n", sizeof(long int));
+	printf("Size of long long int: %i bytes\n", sizeof(long long int));
+	printf("Size of float: %i bytes\n", sizeof(float));
+	return (0);
+}
