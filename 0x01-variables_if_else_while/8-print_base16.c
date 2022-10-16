e70d2524ae4391529d86c2acb4fea1da7bb07ee9
Bobson Obiku <leonineobiku@gmail.com>
2022-10-16 16:11:46 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return:0
+ */
+int main(void)
+{
+	int num;
+	char lc;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (lc = 'a'; lc <= 'f'; lc++)
+		putchar(lc);
+
+	putchar('\n');
+
+	return (0);
+}
