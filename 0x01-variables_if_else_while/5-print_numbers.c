f9c4ff956b1b25bfb73f6ccb8066fc429554b164
Bobson Obiku <leonineobiku@gmail.com>
2022-10-16 06:10:14 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,12 +5,10 @@
  */
 int main(void)
 {
-	char num[10];
+	int num;
 
-	for (num = 0; num < 10; num++)
-	{
-		printf("%s", num);
-	}
+	for (num = 0; num <= 9; num++)
+		printf("%d ", num);
 
 	printf('\n');
 
