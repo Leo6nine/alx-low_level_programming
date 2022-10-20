38364d9d5efd8744df980d66d3c79b825aea1fd6
Bobson Obiku <leonineobiku@gmail.com>
2022-10-20 08:18:16 -0700

    Task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int x, int y, int z);
+void print_remaining_days(int day, int month, int year);
+
+#endif
