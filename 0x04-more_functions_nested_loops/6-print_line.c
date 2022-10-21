e5bdf0b9c45282bc34582a54222ef2a16d8fe91c
Bobson Obiku <leonineobiku@gmail.com>
2022-10-21 16:42:35 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - Print a straiht line
+ * @n: Num of '_'
+ */
+void print_line(int n);
+{
+	int line;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
