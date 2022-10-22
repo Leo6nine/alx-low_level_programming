817c416533a11994ceaa6b2c7fc1709bf7cf6d4f
Bobson Obiku <leonineobiku@gmail.com>
2022-10-21 17:54:32 -0700

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - squar printing
+ * @size: square
+ */
+
+void print_square(int size);
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				-putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
