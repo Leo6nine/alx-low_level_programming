4bb969c357f999a7275b0bcdba12bdf265e47dbf
Bobson Obiku <leonineobiku@gmail.com>
2022-10-21 17:13:26 -0700

    main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,7 +12,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
 int _putchar(char c);
 
 #endif
