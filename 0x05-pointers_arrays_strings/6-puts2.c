5ac5f683c236bde98d6281d0115479ee91b90d73
Bobson Obiku <leonineobiku@gmail.com>
2022-10-24 20:42:12 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - Prints every other char
+ * @str: String
+ * Return: output
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (; tr[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
