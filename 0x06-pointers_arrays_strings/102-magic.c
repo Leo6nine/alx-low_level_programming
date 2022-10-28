5806ef9813ad4822a3ca8601bcd5f1d2eb091fcf
Bobson Obiku <leonineobiku@gmail.com>
2022-10-27 22:31:04 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/** main - Entry point*/
+/**
+ * main - Entry point
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
@@ -10,9 +13,6 @@ int main(void)
 	p = &n;
 	/**
 	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
