18836f5929034c28f171dcc95e193f35674443c2
Bobson Obiku <leonineobiku@gmail.com>
2022-11-02 14:11:47 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,7 +7,7 @@
  * Return: square root or -1.
  */
 
-int power_operation(int n, int c)
+int equation(int n, int c)
 {
 	if (c % (n / c) == 0)
 	{
@@ -16,5 +16,22 @@ int power_operation(int n, int c)
 		else
 			return (-1);
 	}
-	return (0 + power_operation(n, c + 1));
+	return (0 + equation(n, c + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n > 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	return (equation(n, 2));
 }
