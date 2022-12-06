a0aaaad4f63c7f041382b3acdca49bc0ccee8f74
Bobson Obiku <leonineobiku@gmail.com>
2022-12-05 17:14:18 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	ac = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0000);
+	ac = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (ac < 0)
 		return (-1);
 
